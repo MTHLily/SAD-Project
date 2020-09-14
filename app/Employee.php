4d3705648b89c7dc40db41e9391bcc791bfa6ebf
Reynzel Clark Ang <67308347+rccang@users.noreply.github.com>
2020-09-14 14:56:16 +0800

    Created models and migrations

---
+++ app/Employee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'middle_initial',
+        'email_address',
+        'department_id',
+        'status',
+    ];
+
+    public function department(){
+        return $this->belongsTo('App\Department');
+    }
+}
---
+++ app/Peripheral.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Peripheral extends Model
+{
+    protected $fillable = [
+        'asset_tag',
+        'peripheral_name',
+        'setup_id',
+        'peripheral_type',
+        'warranty_id',
+        'status',
+    ];
+
+    public function type(){
+        return $this->belongsTo('App\PeripheralType', 'peripheral_type');
+    }
+    public function warranty(){
+        return $this->belongsTo('App\Warranty');
+    }
+}
