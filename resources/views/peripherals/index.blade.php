098b29e22f3091d935ac025b6142d838e5298006
MTHLily <tree.of.pyres@gmail.com>
2020-09-22 22:22:13 +0900

    Fixed merge issues

--- resources/views/peripherals/index.blade.php
+++ resources/views/peripherals/index.blade.php
@@ -15,7 +15,7 @@
 
 @section('content')
 
-	<a href="/perihperals/create" class="btn btn-success w-100"><i class="fas fa-plus"></i> Add New</a>
+	<a href="/peripherals/create" class="btn btn-success w-100"><i class="fas fa-plus"></i> Add New</a>
 	<table class="table">
 		<thead>
 			<th></th>
@@ -28,7 +28,6 @@
 			<th>Actions</th>
 		</thead>
 		<tbody>
-<<<<<<< HEAD
 			@foreach( $peripherals as $peripheral )
 				<tr>
 					<td>{{$peripheral->asset_tag}}</td>
@@ -40,28 +39,6 @@
 						<div class="d-flex w-100">
 							<a class="btn w-100" href="/peripherals/{{$peripheral->id}}/edit"><i class="fas fa-edit fa-lg"></i></a>
 							<form class="w-100" action="/peripherals/{{$peripheral->id}}" method="POST">
-=======
-			@foreach( $perihperals as $perihperal )
-				<tr>
-					<td><button data-toggle="modal" data-target="#component-{{$component->id}}-info">Open Info</button></td>
-					<td>{{ $component->asset_tag }}</td>
-					<td>{{ $component->component_name }}</td>
-					<td>{{ $component->type()->get()[0]->component_type }}</td>
-					<td>{{ $component->remarks }}</td>
-					<td>{{ $component->issues }}</td>
-					<td>{{ $component->system_id }}</td>
-					<td>
-						{!! ($component->warranty_id != null) ? 
-								"<a href='/warranties/$component->warranty_id'>View Warranty</a>" : 
-								"<a href='/warranties/create'>Assign Warranty</a>"
-						!!}
-					</td>
-					<td>{{ $component->status }}</td>
-					<td>
-						<div class="d-flex w-100">
-							<a class="btn w-100" href="/components/{{$component->id}}/edit"><i class="fas fa-edit fa-lg"></i></a>
-							<form class="w-100" action="/components/{{$component->id}}" method="POST">
->>>>>>> 07c679ec6a24ea85e8a5cb20d8c13d7649708718
 								@method('delete')
 								@csrf
 								<button type="submit" class="btn w-100"><i class="fas fa-trash-alt fa-lg"></i></button>
@@ -73,72 +50,4 @@
 		</tbody>
 	</table>
 
-<<<<<<< HEAD
-=======
-	@foreach( $components as $component )
-
-	<div class="modal" id="component-{{$component->id}}-info">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<h1>Edit Component</h1>
-				<form action="/components/{{$component->id}}" method="POST">
-					@csrf
-					@method('PATCH')
-					<input  type="text" name="asset_tag" placeholder="Asset Tag" 
-							class="form-control" 
-							value="{{$component->asset_tag}}" 
-							required>
-					<input  type="text" name="component_name" placeholder="Name" 
-							class="form-control" 
-							value="{{$component->component_name}}"
-							required>
-					<select name="component_type_id" class="custom-select" required>
-						<option value="1"
-							@if($component->component_type_id == 1)
-							selected
-							@endif
-						>
-							Motherboard
-						</option>
-						<option value="2"
-							@if($component->component_type_id == 2)
-							selected
-							@endif 
-						>
-							CPU
-						</option>
-						<option value="3"
-							@if($component->component_type_id == 3)
-							selected
-							@endif 
-						>
-							GPU
-						</option>
-						<option value="4"
-							@if($component->component_type_id == 4)
-							selected
-							@endif 
-						>
-							RAM
-						</option>
-						<option value="5"
-							@if($component->component_type_id == 5)
-							selected
-							@endif 
-						>
-							Storage
-						</option>
-					</select>
-
-					<button class="btn btn-primary">Edit Item</button>
-					<a class="btn btn-danger" href="/components">Return</a>
-				</form>
-
-			</div>
-		</div>
-	</div>
-
-	@endforeach
-
->>>>>>> 07c679ec6a24ea85e8a5cb20d8c13d7649708718
 @endsection
